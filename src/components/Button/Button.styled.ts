f7a39fcb45f98b27aa549933e4b46bf1449731ca
ycarpenter <yurii.carpenter@gmail.com>
2023-03-24 21:29:48 +0200

    feat: create StyledButton

--- src/components/Button/Button.styled.ts
+++ src/components/Button/Button.styled.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const Button = styled.button`
+export const StyledButton = styled.button`
   border-radius: 50%;
   box-shadow: 0 0 22.5px 0.5px #b0b0b0;
   width: 100px;

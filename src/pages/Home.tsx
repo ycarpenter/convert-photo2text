6e13e5d28f0334832972717960d53fba6c3db864
ycarpenter <yurii.carpenter@gmail.com>
2023-03-24 21:38:14 +0200

    docs: add types

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,21 @@
 import { FormEvent } from 'react'
-import Buttons from '../components/Button/Buttons'
 import FieldFile from '../components/FieldFile/FieldFile'
+import FormButton from '../components/Button/Button'
 import { FormUpload } from './FormUpload.styled'
 
-const sendForm = (e: FormEvent) => {
+const sendForm = (e: FormEvent<HTMLFormElement>) => {
   e.preventDefault()
   console.log(e)
 }
 
-export default function Home() {
+export default function Home(): JSX.Element {
   return (
     <FormUpload
       autoComplete='off'
       onSubmit={sendForm}
     >
       <FieldFile />
-      <Buttons />
+      <FormButton />
     </FormUpload>
   )
 }
